5eed4be38a6a0fd123d6c0ef89d2b2ff623eba1b
heartist98 <mabao.wendy98@gmail.com>
2025-03-12 11:35:42 +0800

    Fixed duplicate heading issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import TodoList from "./components/TodoList.jsx";
-import './App.css';
-
-
+import TodoList from "./components/TodoList.jsx"; 
+import "./index.css";
 
 function App() {
   return (
     <div>
-    {/* Remove this line if the heading is inside TodoList */}
-    {/* <h1>My Todo App</h1> */}
-    <TodoList />
-  </div>
+      <TodoList /> 
+    </div>
   );
 }
 
